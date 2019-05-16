e4ca71c73db0e829cc00d7a1611062e7fad17561
sarika-jammi <50618652+sarika-jammi@users.noreply.github.com>
2019-05-16 10:55:38 +0530

    Add Exception Handling

--- ebay/app/src/test/java/com/app/ebay/performTests/TestEbaySignIn.java
+++ ebay/app/src/test/java/com/app/ebay/performTests/TestEbaySignIn.java
@@ -14,9 +14,13 @@ public class TestEbaySignIn extends BaseTestClass {
      */
     @Test
     public void signInTest(){
-       firstInteractionPage.pressSignIn();
-       signInPage.enterUserName(testDataProvider.getUsr());
-       signInPage.enterPassword(testDataProvider.getPwd());
-       signInPage.pressSignInButton();
+        try{
+             firstInteractionPage.pressSignIn();
+             signInPage.enterUserName(testDataProvider.getUsr());
+             signInPage.enterPassword(testDataProvider.getPwd());
+             signInPage.pressSignInButton();
+        }catch (Exception e){
+           Log.logError(getClass().getName(), "signInTest", "Sign In Unsuccessful");
+        }
     }
 }
